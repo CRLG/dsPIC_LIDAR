4317b4285eb121678a69f319a4fe6c52c23c8afe
NicoFarmer <nicolas.santiago77.deal@gmail.com>
2020-04-04 22:58:50 +0200

    #3 Pins de programmation compatibles autres cartes ELECTROBOT
    Utilisation des pins PGED3 et PGEC3 pour la programmation (pin 4 et 5 du dsPIC) pour compatibilité avec les autres dsPIC utilisés sur la famille ELECTROBOT

--- main.c
+++ main.c
@@ -3,7 +3,7 @@
 // 'C' source line config statements
 
 // FICD
-#pragma config ICS      = PGD1      // ICD Communication Channel Select bits (Communicate on PGEC1 and PGED1)
+#pragma config ICS      = PGD3      // ICD Communication Channel Select bits (Communicate on PGEC3 and PGED3)
 #pragma config JTAGEN   = OFF       // JTAG Enable bit (JTAG is disabled)
 
 // FPOR
